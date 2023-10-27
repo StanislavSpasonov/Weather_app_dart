417088fe1c0e46b6955356301de5e346c56926a4
stanislav spasonov <s.spasonov@itransition.com>
2023-10-27 17:39:37 +0200

    base

---
+++ android/app/src/main/kotlin/com/max/weather/home/weather_home/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.max.weather.home.weather_home
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:weather_home/scr/features/main_page/main_page.dart';
+
+void main() {
+  runApp(const Main());
+}
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
